264960515e4b9e7d28a89361c72a49979f22d37f
patnaik.daiwik@gmail.com <August23#>
2021-07-17 19:38:07 +0530

    1st Version

--- src/pkgVariables/sVariables.scala
+++ src/pkgVariables/sVariables.scala
@@ -2,6 +2,23 @@ package pkgVariables
 
 object sVariables {
   def main(args: Array[String]): Unit = {
-    print("Hello World")
+    //print("Hello World")
+    val str = Seq("Hello", "World")
+      .map(_.toLowerCase)
+      .mkString("[",",","]")
+    println(str)
+
+    var seq: Seq[String] = Seq("My","name","is","Vijay", "K", "Patnaik")
+    seq = Seq("1","2","3")
+
+    print(seq)
+
+    val array: Array[String] = Array("My","name","is","Vijay", "K", "Patnaik")
+
+    var sVar = 6
+    if (sVar < 5){println("Papa") } else { println("Mummy")}
+
+
+
   }
 }
